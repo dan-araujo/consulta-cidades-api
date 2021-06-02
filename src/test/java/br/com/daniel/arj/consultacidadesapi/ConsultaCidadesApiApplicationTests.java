cd40704dc253d163f2d3d28b5f646951d983e587
Daniel Araújo <daniel.ti.info@gmail.com>
2021-06-01 23:32:24 -0300

    Consulta de Cidades API

---
+++ src/test/java/br/com/daniel/arj/consultacidadesapi/ConsultaCidadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.arj.consultacidadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCidadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
