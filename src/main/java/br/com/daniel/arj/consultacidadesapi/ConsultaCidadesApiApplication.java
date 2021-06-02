b213ef431ad11c8fd4f01be40e66c0d26a383ce9
Daniel Araújo <daniel.ti.info@gmail.com>
2021-06-01 23:26:02 -0300

    Classe principal

---
+++ src/main/java/br/com/daniel/arj/consultacidadesapi/ConsultaCidadesApiApplication.java
@@ -0,0 +1,34 @@
+package br.com.daniel.arj.consultacidadesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCidadesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCidadesApiApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
